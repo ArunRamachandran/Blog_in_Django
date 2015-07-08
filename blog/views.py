0d0bebf890d6da61efaf9bfb20609ce1dd6fc793
Arun Ramachandran <arunkramachandran92@gmail.com>
2015-07-08 08:59:35 +0000

    Added templates and style

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-  return render(request, 'blog/post_list.html', {})
+    print "Request : ", request
+    posts = Post.objects.all()
+    
+    return render(request, 'blog/post_list.html', {'posts': posts})
